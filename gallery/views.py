50f1589561b775a7c94a5db88e01102ecb31b7c6
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-10-10 11:35:50 +0300

    Addeed the views file

---
+++ gallery/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import Image, Location
+
+# Create your views here
+def home(request):
+
+    images = Image.get_images()
+    locations = Location.get_locations()
+ 
+    return render(request, 'gallery/home.html', {'images':images, 'locations':locations})
+
+
+def image_location(request, location):
+    images = Image.filter_by_location(location)
+    
+    return render(request, 'gallery/location.html', {'images': images})
+
+
+def search_results(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        
+        return render(request, 'gallery/search_results.html', {"message": message, "images": searched_images})
+    else:
+        message = "There are no images in that category yet"
+        return render(request, 'gallery/search_results.html', {"message": message})
+
